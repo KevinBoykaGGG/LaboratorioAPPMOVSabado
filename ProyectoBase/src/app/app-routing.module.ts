d0f2ee9f281b3b29684405e39fbb8aaa6bd66fde
kevin Najera <kevinalex360boyka@gmail.com>
2019-10-12 08:58:22 -0500

    Login funcional

--- ProyectoBase/src/app/app-routing.module.ts
+++ ProyectoBase/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import {RegistrarPage} from './auth/registrar/registrar.page'
+import { RegistrarPage } from './auth/registrar/registrar.page';
+import { LoginPage } from './auth/login/login.page';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: 'registrar', component: RegistrarPage },
-  { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' },  { path: 'registro', loadChildren: './auth/registro/registro.module#RegistroPageModule' }
-
+  { path: 'registro', component: RegistrarPage },
+  { path: 'registrar', component: RegistrarPage  },
+  { path: 'login', component: LoginPage },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
 
 ];
 @NgModule({
--- ProyectoBase/src/app/model/usuario.ts
+++ ProyectoBase/src/app/model/usuario.ts
@@ -2,7 +2,7 @@
 import { HttpService } from '../service/http-service.service';
 
 export class Usuario {
-    public static endPoint = "/usuario";
+    public static endPoint = '/usuario';
     public nombre: string;
     public correo: string;
     public contrasena: string;
